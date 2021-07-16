fd2c37504e01cd5fb60e05663e1204c1e20f460c
Mike0298 <38065113+Mike0298@users.noreply.github.com>
2021-07-17 00:43:54 +1000

    add domain option

--- urlshort.js
+++ urlshort.js
@@ -2,6 +2,19 @@ let form = document.getElementById("form");
 let button = document.getElementById("button");
 let urlDiv = document.getElementById("resultUrl");
 
+let option1 = document.getElementById("option1");
+let option2 = document.getElementById("option2");
+
+let choosen = 1;
+
+option1.addEventListener("click", () => {
+  choosen = 1;
+});
+
+option2.addEventListener("click", () => {
+  choosen = 2;
+});
+
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
   urlDiv.href = "";
@@ -14,7 +27,16 @@ form.addEventListener("submit", async (e) => {
     button.disabled = true;
     button.value = "Loading...";
     res = await axios.get(`https://api.shrtco.de/v2/shorten?url=${url}`);
-    resultUrl = res.data.result.full_short_link;
+    switch (choosen) {
+      case 1:
+        resultUrl = res.data.result.full_short_link;
+        break;
+      case 2:
+        resultUrl = res.data.result.full_short_link2;
+        break;
+      default:
+        resultUrl = res.data.result.full_short_link;
+    }
     console.log(resultUrl);
     urlDiv.href = resultUrl;
     urlDiv.innerHTML = resultUrl;
